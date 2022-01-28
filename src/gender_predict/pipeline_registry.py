d1f8638a62f126bac1deb0acb8ece483208ef57f
pierrehanne <43729077+pierrehanne@users.noreply.github.com>
2022-01-28 22:24:39 +0100

    init data engineering pipeline

--- src/gender_predict/pipeline_registry.py
+++ src/gender_predict/pipeline_registry.py
@@ -3,6 +3,7 @@ from typing import Dict
 
 from kedro.pipeline import Pipeline
 
+from .pipelines import data_engineering as de
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -10,4 +11,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    return {"__default__": Pipeline([])}
+    # instantiate data engineering pipeline
+    pipeline_data_engineering = de.create_pipeline()
+    
+    return {"__default__": Pipeline([pipeline_data_engineering])}
