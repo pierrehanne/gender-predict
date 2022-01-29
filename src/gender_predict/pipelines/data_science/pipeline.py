5d2798c7fa26730ac359050c5c7a143b61b4b15a
pierrehanne <43729077+pierrehanne@users.noreply.github.com>
2022-01-30 00:03:23 +0100

    init pytorch model architecture

--- src/gender_predict/pipelines/data_science/pipeline.py
+++ src/gender_predict/pipelines/data_science/pipeline.py
@@ -7,6 +7,8 @@ from kedro.pipeline import Pipeline, node
 
 from .nodes import (
     split_data,
+    init_device,
+    init_neural_network
 )
 
 def create_pipeline(**kwargs):
@@ -17,6 +19,18 @@ def create_pipeline(**kwargs):
                 inputs=["model_input_table", "parameters"],
                 outputs=["X_train", "X_test", "y_train", "y_test"],
                 name="splitted_data_node",
+            ),
+            node(
+                func=init_device,
+                inputs="parameters",
+                outputs="device",
+                name="init_device_node",
+            ),
+            node(
+                func=init_neural_network,
+                inputs=["device", "parameters"],
+                outputs="model",
+                name="init_neural_network_node",
             ),
         ]
     )
